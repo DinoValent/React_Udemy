73f0dd9a1a349c4bbb5f4165f42abe3d9e6099a8
DinoValent <gamerdino41@gmail.com>
2023-10-12 12:51:49 -0300

    UseState

--- 03-counter-app/src/CouterApp.jsx
+++ 03-counter-app/src/CouterApp.jsx
@@ -1,3 +1,13 @@
-export const CounterApp = () => {
-  return <></>;
+import { useState } from "react";
+
+export const CounterApp = ({ value }) => {
+  const [counter, setCounter] = useState(value);
+  const counterIncrement = () => setCounter((c) => c + 1);
+
+  return (
+    <>
+      <h2>El valor del contador es {counter}</h2>
+      <button onClick={counterIncrement}>Incrementar contador +1</button>
+    </>
+  );
 };
--- 03-counter-app/src/main.jsx
+++ 03-counter-app/src/main.jsx
@@ -5,6 +5,6 @@ import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <CounterApp />
+    <CounterApp value={7} />
   </React.StrictMode>
 );
